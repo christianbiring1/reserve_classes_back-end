dbac10e64d714cc64ecedaf45b264fdc749894f6
Jake Felice <jasonfelice0@gmail.com>
2022-10-05 23:12:58 -0700

    Create group routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
   resources :registrations, only:[:create]
+  resources :groups, only:[:index, :show, :create, :destroy]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
   root to: 'static#home'
