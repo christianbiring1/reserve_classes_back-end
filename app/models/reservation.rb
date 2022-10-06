31dd8dd85c4e9acd560ce6664e1f761e3d675596
Jake Felice <jasonfelice0@gmail.com>
2022-10-05 23:35:56 -0700

    Fix rubocop offenses

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
-  belongs_to :user, foreign_key: "user_id"
+  belongs_to :user, foreign_key: 'user_id'
 
-  belongs_to :group, foreign_key: "group_id"
+  belongs_to :group, foreign_key: 'group_id'
 end
