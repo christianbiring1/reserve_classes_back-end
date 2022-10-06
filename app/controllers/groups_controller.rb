e9473032de45db3b21ca60332eed4bc1e9a45529
Jake Felice <jasonfelice0@gmail.com>
2022-10-05 22:31:37 -0700

    Create a create method for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,4 +8,13 @@ class GroupsController < ApplicationController
     group = Group.find(params[:id])
     render json: group
   end
+
+  def create
+    group = @current_user.groups.new(groups_params)
+    if group.save
+      render json: group, status: :created
+    else
+      render json: group.errors.full_messages, status: :unprocessable_entity
+    end
+  end
 end
