0105b88a3405e79e5bad13c69c91411591e895c6
Jake Felice <jasonfelice0@gmail.com>
2022-10-05 23:22:36 -0700

    Set current user in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = User.find(session[:user_id]) if session[:user_id]
+  end
 end
