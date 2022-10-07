ce15425b65d91567170e063786093ad96423aa50
Jake Felice <jasonfelice0@gmail.com>
2022-10-06 23:30:53 -0700

    Fix models conflict

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-class Group < ApplicationRecord
-  belongs_to :user, foreign_key: 'user_id'
-  has_many :reservations
-end
-=======
 class Group < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   has_many :reservations
   validates :name, :title, presence: true
 end
->>>>>>> develop
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-class Reservation < ApplicationRecord
-  belongs_to :user, foreign_key: 'user_id'
-
-  belongs_to :group, foreign_key: 'group_id'
-end
-=======
 class Reservation < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   belongs_to :group, foreign_key: 'group_id'
 end
->>>>>>> develop
