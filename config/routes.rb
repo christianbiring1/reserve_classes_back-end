119d65d1393e8695c208d802187084cfde24ae2f
Jake Felice <jasonfelice0@gmail.com>
2022-10-06 23:31:57 -0700

    Fix routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
   resources :registrations, only:[:create]
-<<<<<<< HEAD
   resources :groups, only:[:index, :show, :create, :destroy]
-=======
   resources :reservations, only:[:index, :create]
->>>>>>> develop
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
   # for reservations
