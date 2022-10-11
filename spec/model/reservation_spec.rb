ed17f585428ef56d1500cdc51776eb184bf4909a
christian <christianbiringanine22@gmail.com>
2022-10-11 16:47:27 +0200

    Associations rspec resevrations beloning to user, and a class

--- spec/model/reservation_spec.rb
+++ spec/model/reservation_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
+  it 'Should belong to a user' do
+    assc = described_class.reflect_on_association(:user)
+    expect(assc.macro).to eq :belongs_to
+  end
+
+  it 'Should belong to a group' do
+    assc = described_class.reflect_on_association(:group)
+    expect(assc.macro).to eq :belongs_to
+  end
 end
