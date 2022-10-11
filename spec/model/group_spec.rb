2f8d28f8602b86d99310311c126736616c516931
christian <christianbiringanine22@gmail.com>
2022-10-11 17:03:32 +0200

    Add class validates presence_of photo and title

---
+++ spec/model/group_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+
+  before :each do
+    @user = User.create(name: 'Tom', email: 'test@gmail.com', password: '1234')
+  end
+  
+  it 'Should have the image and title attribute' do
+    group = Group.create!(image: 'https://unsplash/image.com', title: 'Sport', user_id: @user.id)
+    group.image = nil
+    expect(group).to_not be_valid
+  end
+  it 'Should belong to a user' do
+    assc = described_class.reflect_on_association(:user)
+    expect(assc.macro).to eq :belongs_to
+  end
+
+  it 'Should have many reservations' do
+    assc = described_class.reflect_on_association(:reservations)
+    expect(assc.macro).to eq :has_many
+  end
+end
