263daaa486c5a5f5c7eda2f69458edf26d3a79c7
christian <christianbiringanine22@gmail.com>
2022-10-11 16:44:07 +0200

    Associations rspec user with reservations and classes

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -21,4 +21,14 @@ RSpec.describe User, type: :model do
     @invalid_user.email = 'test@gmail.com'
     expect(@invalid_user.valid?).to be_truthy
   end
+
+  it 'Should have many groups' do
+    assc = described_class.reflect_on_association(:groups)
+    expect(assc.macro).to eq :has_many
+  end
+
+  it 'Should have many reservations' do
+    assc = described_class.reflect_on_association(:reservations)
+    expect(assc.macro).to eq :has_many
+  end
 end
