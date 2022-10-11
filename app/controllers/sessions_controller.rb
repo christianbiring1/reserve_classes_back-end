71bd60d3161fe11cfde9b08144940e7f1f541687
Jake Felice <jasonfelice0@gmail.com>
2022-10-10 23:50:27 -0700

    Update json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,17 @@ class SessionsController < ApplicationController
 
   def create
     user = User
-      .find_by(email: params['user']['email'])
-      .try(:authenticate, params['user']['password'])
+      .find_by(email: params[:email])
+      .try(:authenticate, params[:password])
     if user
       session[:user_id] = user.id
       render json: {
-        status: :created,
+        status: "Logged in successfully!",
         logged_in: true,
         user:
       }
     else
-      render json: { status: 401 }
+      render json: { status: "Incorrect email or password." }
     end
   end
 
