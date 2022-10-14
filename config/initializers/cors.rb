d231db12e43d56a6d8c00722d256fa21f351fc43
Jake Felice <jasonfelice0@gmail.com>
2022-10-13 21:31:02 -0700

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
-
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  end
-
-  allow do
-    origins "http://productionlink.herokuapp.com"
+    origins "https://6348e50f95e5525da6d1c6e5--scintillating-liger-4d35b6.netlify.app"
 
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
